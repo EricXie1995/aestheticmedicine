4df58a3779d96e0f0e43aee9c881230e026f92d7
Eric <eric1995840807@gmail.com>
2021-06-12 13:16:21 +0800

    initial commit

---
+++ src/main/java/com/iiiedu/beauty/AestheticmedicineApplication.java
@@ -0,0 +1,13 @@
+package com.iiiedu.beauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestheticmedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AestheticmedicineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iiiedu/aestheticmedicine/AestheticmedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiiedu.aestheticmedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AestheticmedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
