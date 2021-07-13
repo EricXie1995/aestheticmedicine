61c880e163ada4addd5a725af7f46cc8ff811c46
evelyn0324 <cindy3837@gmail.com>
2021-07-13 11:31:14 +0800

    預約完成查詢

--- src/main/java/com/iiiedu/beauty/appointment/service/AppointmentCityService.java
+++ src/main/java/com/iiiedu/beauty/appointment/service/AppointmentCityService.java
@@ -2,11 +2,9 @@ package com.iiiedu.beauty.appointment.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.iiiedu.beauty.model.City;
 
-@Service
+
 public interface AppointmentCityService {
 	
 
--- src/main/java/com/iiiedu/beauty/model/City.java
+++ src/main/java/com/iiiedu/beauty/model/City.java
@@ -11,6 +11,9 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "City")
 public class City {
